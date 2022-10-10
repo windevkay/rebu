fbafd1ea97920e21d93b17edcdcad3ca8429aac9
Kola Adebayo <kolaadebayo@Kolas-MacBook-Pro.local>
2022-10-10 16:50:22 -0600

    refactor into services

---
+++ services/services.go
@@ -0,0 +1,55 @@
+package services
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/windevkay/rebu/structs"
+)
+
+type Driver structs.Driver
+
+func convertStringHelper (value string) float32 {
+	result, err := strconv.ParseFloat(value, 32)
+	if err != nil {
+		panic("Error converting cost data...terminating 😓")
+	}
+	return float32(result)
+}
+/*
+* Returns an array of drivers based on requested range
+* The requested range can be incremented to widen a search
+*/
+func GetDriversInRange(requestedRange float32) []Driver {
+	file, err := os.Open("./data.txt")
+	if err != nil {
+		panic("Error reading driver data...terminating 😓")
+	}
+
+	fileReader := bufio.NewReader(file)
+	var drivers []Driver
+
+	for {
+		content, err := fileReader.ReadString('\n')
+		data := strings.Split(content, ",")
+
+		driverDistance := convertStringHelper(data[1])
+		if driverDistance <= requestedRange {
+			driver := Driver{
+				Id: data[0],
+				Cost: driverDistance,
+				CostBeforeUnoccupied: convertStringHelper(data[2]),
+			}
+	
+			drivers = append(drivers, driver)
+		}
+
+		if err == io.EOF {
+			break
+		}
+	}
+	return drivers
+}
--- structs/structs.go
+++ structs/structs.go
@@ -1,13 +1,5 @@
 package structs
 
-import (
-	"bufio"
-	"io"
-	"os"
-	"strconv"
-	"strings"
-)
-
 type Driver struct {
 	Id string
 	//the cost represents how far the driver is from the rider
@@ -29,41 +21,3 @@ func (driver *Driver) FreeDriver() {
 	driver.CostBeforeUnoccupied = 0
 }
 
-func convertStringHelper (value string) float32 {
-	result, err := strconv.ParseFloat(value, 32)
-	if err != nil {
-		panic("Error converting cost data...terminating 😓")
-	}
-	return float32(result)
-}
-/*
-* Returns an array of drivers based on requested range
-* The requested range can be incremented to widen a search
-*/
-func GetDriversInRange(requestedRange float32) []Driver {
-	file, err := os.Open("./data.txt")
-	if err != nil {
-		panic("Error reading driver data...terminating 😓")
-	}
-
-	fileReader := bufio.NewReader(file)
-	var drivers []Driver
-
-	for {
-		content, err := fileReader.ReadString('\n')
-		data := strings.Split(content, ",")
-
-		driver := Driver{
-			Id: data[0],
-			Cost: convertStringHelper(data[1]),
-			CostBeforeUnoccupied: convertStringHelper(data[2]),
-		}
-
-		drivers = append(drivers, driver)
-
-		if err == io.EOF {
-			break
-		}
-	}
-	return drivers
-}
